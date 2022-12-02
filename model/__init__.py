5a85cd8999d0513f5a05c39fbdeba993c6c042f8
Makenson Noel <makensonnoel@Makensons-Air.attlocal.net>
2022-12-02 14:22:44 -0500

    ASL Alphabet Update, A,B,C,D,E,F,G,H,I,L

--- model/__init__.py
+++ model/__init__.py
@@ -1,2 +1,7 @@
 from model.keypoint_classifier.keypoint_classifier import KeyPointClassifier
 from model.point_history_classifier.point_history_classifier import PointHistoryClassifier
+
+'''Open
+Close
+Pointer
+OK'''
